bd3d4ed128434ae3fe76f6cfc1657428f64f86bf
ext_em <edouard.misset@intest.info>
2023-05-10 16:38:36 +0200

    fix: all props or none

--- src/04-advanced-components/26-either-all-these-props-or-none.problem.tsx
+++ src/04-advanced-components/26-either-all-these-props-or-none.problem.tsx
@@ -1,14 +1,17 @@
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler } from 'react'
 
 type InputProps = (
   | {
-      value: string;
-      onChange: ChangeEventHandler;
+      value: string
+      onChange: ChangeEventHandler
     }
-  | {}
+  | {
+      value?: undefined
+      onChange?: undefined
+    }
 ) & {
-  label: string;
-};
+  label: string
+}
 
 export const Input = ({ label, ...props }: InputProps) => {
   return (
@@ -18,8 +21,8 @@ export const Input = ({ label, ...props }: InputProps) => {
         <input {...props} />
       </label>
     </div>
-  );
-};
+  )
+}
 
 export const Test = () => {
   return (
@@ -33,5 +36,5 @@ export const Test = () => {
       {/* @ts-expect-error */}
       <Input label="Greeting" onChange={() => {}} />
     </div>
-  );
-};
+  )
+}
