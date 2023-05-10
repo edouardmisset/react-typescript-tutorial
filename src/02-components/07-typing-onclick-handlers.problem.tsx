91a8bc51240eaa9615b76c361dd8f6bdbd5d0cb8
ext_em <edouard.misset@intest.info>
2023-05-10 15:40:01 +0200

    fix: ex 7 - onClick

--- src/02-components/07-typing-onclick-handlers.problem.tsx
+++ src/02-components/07-typing-onclick-handlers.problem.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from 'react'
+import { MouseEventHandler } from 'react'
 
 interface ButtonProps {
-  className: string;
-  children: React.ReactNode;
+  className: string
+  children: React.ReactNode
+  onClick: MouseEventHandler | undefined
 }
 
 export const Button = ({ children, className, onClick }: ButtonProps) => {
@@ -10,5 +12,5 @@ export const Button = ({ children, className, onClick }: ButtonProps) => {
     <button onClick={onClick} className={className}>
       {children}
     </button>
-  );
-};
+  )
+}
