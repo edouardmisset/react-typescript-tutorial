af52fbd9a3cf833ced20126312954066e8685983
ext_em <edouard.misset@intest.info>
2023-05-10 17:25:49 +0200

    fix: generic localStorage hooks with zod

--- src/05-advanced-hooks/35-generic-localstorage-hook-with-zod.problem.tsx
+++ src/05-advanced-hooks/35-generic-localstorage-hook-with-zod.problem.tsx
@@ -1,35 +1,35 @@
-import { it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
-import { z } from "zod";
+import { it } from 'vitest'
+import { Equal, Expect } from '../helpers/type-utils'
+import { ZodSchema, z } from 'zod'
 
-export const useLocalStorage = <T,>(prefix: string, schema: any) => {
-  const schemaOrNull = schema.nullable();
+export const useLocalStorage = <T,>(prefix: string, schema: ZodSchema<T>) => {
+  const schemaOrNull = schema.nullable()
   return {
     get: (key: string): T | null => {
       return schemaOrNull.parse(
-        JSON.parse(window.localStorage.getItem(prefix + key) || "null"),
-      );
+        JSON.parse(window.localStorage.getItem(prefix + key) || 'null'),
+      )
     },
     set: (key: string, value: T) => {
-      window.localStorage.setItem(prefix + key, JSON.stringify(value));
+      window.localStorage.setItem(prefix + key, JSON.stringify(value))
     },
-  };
-};
+  }
+}
 
-const user = useLocalStorage("user", z.object({ name: z.string() }));
+const user = useLocalStorage('user', z.object({ name: z.string() }))
 
-it("Should let you set and get values", () => {
-  user.set("matt", { name: "Matt" });
+it('Should let you set and get values', () => {
+  user.set('matt', { name: 'Matt' })
 
-  const mattUser = user.get("matt");
+  const mattUser = user.get('matt')
 
-  type tests = [Expect<Equal<typeof mattUser, { name: string } | null>>];
-});
+  type tests = [Expect<Equal<typeof mattUser, { name: string } | null>>]
+})
 
-it("Should not let you set a value that is not the same type as the type argument passed", () => {
+it('Should not let you set a value that is not the same type as the type argument passed', () => {
   user.set(
-    "something",
+    'something',
     // @ts-expect-error
     {},
-  );
-});
+  )
+})
