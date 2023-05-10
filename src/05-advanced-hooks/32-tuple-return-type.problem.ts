cb09460d3a06669bd38509a1aa39216c6440953b
ext_em <edouard.misset@intest.info>
2023-05-10 17:09:27 +0200

    fix: tuple retrun type

--- src/05-advanced-hooks/32-tuple-return-type.problem.ts
+++ src/05-advanced-hooks/32-tuple-return-type.problem.ts
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import { useState } from "react"
+import { Equal, Expect } from "../helpers/type-utils"
 
 export const useId = (defaultId: string) => {
-  const [id, setId] = useState(defaultId);
+  const [id, setId] = useState(defaultId)
 
-  return [id, setId];
-};
+  return [id, setId] as const
+}
 
-const [id, setId] = useId("1");
+const [id, setId] = useId("1")
 
 type tests = [
   Expect<Equal<typeof id, string>>,
   Expect<Equal<typeof setId, React.Dispatch<React.SetStateAction<string>>>>,
-];
+]
