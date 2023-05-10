146250fe4fbb945c0092cfdc4411e2d1df02cf5d
ext_em <edouard.misset@intest.info>
2023-05-10 15:51:19 +0200

    fix: ex 10 - props from custom components

--- src/02-components/10-extracting-props-from-custom-components.problem.tsx
+++ src/02-components/10-extracting-props-from-custom-components.problem.tsx
@@ -1,26 +1,27 @@
 // Imagine NavBar is an external library!
 
 export const NavBar = (props: {
-  title: string;
-  links: string[];
-  children: React.ReactNode;
+  title: string
+  links: string[]
+  children: React.ReactNode
 }) => {
-  return <div>Some content</div>;
-};
+  return <div>Some content</div>
+}
 
+import { ComponentProps } from 'react'
 // Your app:
 
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
-type NavBarProps = unknown;
+type NavBarProps = ComponentProps<typeof NavBar>
 
 type test = Expect<
   Equal<
     NavBarProps,
     {
-      title: string;
-      links: string[];
-      children: React.ReactNode;
+      title: string
+      links: string[]
+      children: React.ReactNode
     }
   >
->;
+>
