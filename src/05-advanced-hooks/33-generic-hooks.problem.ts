9413412d5c21a856b303eac9d5f960d9fc16f2b9
ext_em <edouard.misset@intest.info>
2023-05-10 17:13:07 +0200

    fix: generic hooks

--- src/05-advanced-hooks/33-generic-hooks.problem.ts
+++ src/05-advanced-hooks/33-generic-hooks.problem.ts
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import { useState } from "react"
+import { Equal, Expect } from "../helpers/type-utils"
 
-export const useStateAsObject = (initial: any) => {
-  const [value, set] = useState(initial);
+export const useStateAsObject = <T>(initial: T) => {
+  const [value, set] = useState(initial)
 
   return {
     value,
     set,
-  };
-};
+  }
+}
 
-const example = useStateAsObject({ name: "Matt" });
+const example = useStateAsObject({ name: "Matt" })
 
 type ExampleTests = [
   Expect<Equal<typeof example.value, { name: string }>>,
@@ -20,11 +20,11 @@ type ExampleTests = [
       React.Dispatch<React.SetStateAction<{ name: string }>>
     >
   >,
-];
+]
 
-const num = useStateAsObject(2);
+const num = useStateAsObject(2)
 
 type NumTests = [
   Expect<Equal<typeof num.value, number>>,
   Expect<Equal<typeof num.set, React.Dispatch<React.SetStateAction<number>>>>,
-];
+]
