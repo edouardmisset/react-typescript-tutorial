93f0942965d754978d38cac92a36063541caf412
ext_em <edouard.misset@intest.info>
2023-05-10 15:48:06 +0200

    fix: ex 9 - html props with one changed

--- src/02-components/09-html-props-with-one-changed.problem.tsx
+++ src/02-components/09-html-props-with-one-changed.problem.tsx
@@ -1,27 +1,29 @@
-import { ComponentProps } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import { ComponentProps } from 'react'
+import { Equal, Expect } from '../helpers/type-utils'
 
 export const Input = (
-  props: ComponentProps<"input"> & { onChange: (value: string) => void }
+  props: Omit<ComponentProps<'input'>, 'onChange'> & {
+    onChange: (value: string) => void
+  },
 ) => {
   return (
     <input
       {...props}
-      onChange={(e) => {
-        props.onChange(e.target.value);
+      onChange={e => {
+        props.onChange(e.target.value)
       }}
     ></input>
-  );
-};
+  )
+}
 
 const Parent = () => {
   return (
     <Input
-      onChange={(e) => {
-        console.log(e);
+      onChange={e => {
+        console.log(e)
 
-        type test = Expect<Equal<typeof e, string>>;
+        type test = Expect<Equal<typeof e, string>>
       }}
     ></Input>
-  );
-};
+  )
+}
