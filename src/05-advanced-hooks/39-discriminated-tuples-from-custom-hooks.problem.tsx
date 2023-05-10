8bd75fd048b21e559267304aa64eb2b9178712c1
ext_em <edouard.misset@intest.info>
2023-05-10 17:31:50 +0200

    fix: discriminated tuples custom hooks

--- src/05-advanced-hooks/39-discriminated-tuples-from-custom-hooks.problem.tsx
+++ src/05-advanced-hooks/39-discriminated-tuples-from-custom-hooks.problem.tsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-export type Result<T> = [
-  "loading" | "success" | "error",
-  T | Error | undefined,
-];
+export type Result<T> =
+  | ['loading', undefined]
+  | ['success', T]
+  | ['error', Error]
 
 export const useData = <T,>(url: string): Result<T> => {
-  const [result, setResult] = useState<Result<T>>(["loading", undefined]);
+  const [result, setResult] = useState<Result<T>>(['loading', undefined])
 
   useEffect(() => {
     fetch(url)
-      .then((response) => response.json())
-      .then((data) => setResult(["success", data]))
-      .catch((error) => setResult(["error", error]));
-  }, [url]);
+      .then(response => response.json())
+      .then(data => setResult(['success', data]))
+      .catch(error => setResult(['error', error]))
+  }, [url])
 
-  return result;
-};
+  return result
+}
 
 const Component = () => {
   const [status, value] = useData<{ title: string }>(
-    "https://jsonplaceholder.typicode.com/todos/1",
-  );
+    'https://jsonplaceholder.typicode.com/todos/1',
+  )
 
-  if (status === "loading") {
-    return <div>Loading...</div>;
+  if (status === 'loading') {
+    return <div>Loading...</div>
   }
 
-  if (status === "error") {
-    return <div>Error: {value.message}</div>;
+  if (status === 'error') {
+    return <div>Error: {value.message}</div>
   }
 
-  return <div>{value.title}</div>;
-};
+  return <div>{value.title}</div>
+}
